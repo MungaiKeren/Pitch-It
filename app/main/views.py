c4e30b8fa3e97b1bf734c786b60c576058e4c49b
MungaiKeren <wambukeren@gmail.com>
2019-09-15 12:52:27 +0300

    import blueprints

---
+++ app/main/views.py
@@ -0,0 +1,18 @@
+from flask import render_template, redirect,request,url_for,abort
+from . import main
+from flask_login import login_required
+from ..models import User
+
+@main.route('/')
+@login_required
+def index():
+    return render_template('index.html')
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+    return render_template("profiles.html",user=user)    
