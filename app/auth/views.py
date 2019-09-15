60ae0f24dbf4518d6735015718b3fb4e95077655
MungaiKeren <wambukeren@gmail.com>
2019-09-15 15:25:00 +0300

    create the logout user function

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,7 +4,7 @@ from flask import render_template,redirect,request,url_for
 from ..models import User
 from .forms import RegistrationForm,LoginForm
 from .. import db
-from flask_login import login_user
+from flask_login import login_user,logout_user,login_required
 
 @auth.route('/login')
 def login():
@@ -35,3 +35,9 @@ def login():
 
     title = "Pitch-it login"
     return render_template('auth/login.html',login_form = login_form,title=title)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
