9408bd51b5254761730369afda19572ae789802f
MungaiKeren <wambukeren@gmail.com>
2019-09-15 16:49:19 +0300

    Edit more errors in files

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,26 +1,10 @@
-from flask import render_template,redirect,request,url_for,flash
+from flask import render_template,request,redirect, url_for, flash
 from . import auth
-from flask import render_template,redirect,request,url_for
 from ..models import User
 from .forms import RegistrationForm,LoginForm
 from .. import db
 from flask_login import login_user,logout_user,login_required
 
-@auth.route('/login')
-def login():
-    return render_template('auth/login.html')
-
-@auth.route('/register',methods = ['GET','POST'])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(email = form.email.data, username=form.username.data,password = form.password.data)
-        db.session.add(user)
-        db.session.commit()
-
-        return redirect(url_for('auth.login'))
-        title = "New account"
-    return render_template('auth/register.html',registration_form=form)
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -36,8 +20,21 @@ def login():
     title = "Pitch-It Login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('auth.login'))
+        title = "New account"
+    return render_template('auth/register.html',registration_form = form)
+
+
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('main.index'))
+    return redirect(url_for("main.index"))
