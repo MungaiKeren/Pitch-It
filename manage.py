8009b5935846b9c51ac381c5d841defb72e0ee43
MungaiKeren <wambukeren@gmail.com>
2019-09-15 13:23:03 +0300

    This commit uses werkzeug  security to hash passwords

--- manage.py
+++ manage.py
@@ -1,12 +1,16 @@
 from app import create_app,db
 from app.models import User,Role
+
+from  flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager,Shell,Server
 
 # Creating app instance
 app = create_app('development')
 
 manager = Manager(app)
+migrate = Migrate(app,db)
 
+manager.add_command('db',MigrateCommand)
 manager.add_command('server',Server)
 
 @manager.command
