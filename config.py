a2912cc7fd0bd5439fb23c067acc795daf36ccef
MungaiKeren <wambukeren@gmail.com>
2019-09-15 21:19:58 +0300

    create an email instance for sending emails to our user

--- config.py
+++ config.py
@@ -4,6 +4,13 @@ class Config:
     SECRET_KEY='ercfgvbniojnngfdfgssdfghjsdfghdfg'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://keren:kayren@localhost/ppitch'
     UPLOADED_PHOTOS_DEST='app/static/photos'
+    # email configurations
+    MAIL_SERVER = 'stmp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
 
     @staticmethod
     def init_app(app):
---
+++ start.sh
@@ -0,0 +1,5 @@
+export SECRET_KEY='ercfgvbniojnngfdfgssdfghjsdfghdfg'
+export MAIL_USERNAME='wambukeren@gmail.com'
+export MAIL_PASSWORD='kerenwamungai3839'
+
+python3.6 manage.py server
