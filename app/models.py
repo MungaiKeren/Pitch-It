73d4486e4994a76e7b38f75f625f56bfe8939390
MungaiKeren <wambukeren@gmail.com>
2019-09-15 17:28:52 +0300

    create and remane profiles.html files

--- app/models.py
+++ app/models.py
@@ -1,13 +1,13 @@
 from . import db
-from . import LoginManager
-from werkzeug.security import generate_password_hash,check_password_hash
+from . import login_manager
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash,check_password_hash
 
 
-
-#@login_manager.user_loader
+@login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
 
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
