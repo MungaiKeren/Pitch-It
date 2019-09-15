5db4d447b91516889e6bdfd4ac127b5ac70dbdfc
MungaiKeren <wambukeren@gmail.com>
2019-09-15 12:33:17 +0300

    add initial folder structure

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+    DEBUG = True
+    SECRET_KEY = 'xydcfgvdfgxcvbxcvrtyxcvfgjnnnbgvvvhjgv'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://keren:kayren@localhost:5432/pitchit'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
